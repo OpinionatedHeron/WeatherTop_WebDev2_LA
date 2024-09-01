e5cc9034510db392ea7fb85e85a8d3be017a8d68
OpinionatedHeron <leamoy@gmail.com>
2024-09-01 02:49:01 +0100

    Adding Accounts & Member Login

--- routes.js
+++ routes.js
@@ -3,17 +3,22 @@ import { dashboardController } from "./controllers/dashboard-controller.js";
 import { aboutController } from "./controllers/about-controller.js";
 import { homeController } from "./controllers/home-controller.js";
 import { stationController } from "./controllers/station-controller.js";
+import { accountsController } from './controllers/accounts-controller.js';
 
 export const router = express.Router();
 
-router.get("/", dashboardController.index);
-router.get("/home", homeController.index);
+router.get("/", accountsController.index);
+router.get("/login", accountsController.login);
+router.get("/signup", accountsController.signup);
+router.get("/logout", accountsController.logout);
+router.post("/register", accountsController.register);
+router.post("/authenticate", accountsController.authenticate);
+
 router.get("/dashboard", dashboardController.index);
-router.get("/about", aboutController.index);
-
 router.post("/dashboard/addstation", dashboardController.addStation);
-
+router.get("/dashboard/deletestation/:id", dashboardController.deleteStation);
 router.get("/station/:id", stationController.index);
 router.post("/station/:id/addreport", stationController.addReport);
-router.get("/dashboard/deletestation/:id", stationController.deleteStation);
 router.get("/station/:stationid/deletereport/:reportid", stationController.deleteReport);
+router.get("/about", aboutController.index);
+router.get("/home", homeController.index);
