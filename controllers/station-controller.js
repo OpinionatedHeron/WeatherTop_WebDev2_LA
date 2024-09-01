e812775bd2850d24580bb5d8928441d3fabaf9a2
OpinionatedHeron <leamoy@gmail.com>
2024-09-01 01:00:39 +0100

    Adding Reports and Forms

--- controllers/station-controller.js
+++ controllers/station-controller.js
@@ -1,4 +1,5 @@
 import { stationStore } from "../models/station-store.js";
+import { reportStore } from "../models/report-store.js";
 
 export const stationController = {
   async index(request, response) {
@@ -8,5 +9,18 @@ export const stationController = {
       station: station,
     };
     response.render("station-view", viewData);
+  },
+
+  async addReport(request, response) {
+    const station = await stationStore.getStationById(request.params.id);
+    const newReport = {
+      code: request.body.code,
+      temp: request.body.temp,
+      windSpeed: request.body.windSpeed,
+      pressure: request.body.pressure,
+    };
+    console.log(`adding report ${newReport.title}`);
+    await reportStore.addReport(station._id, newReport);
+    response.redirect("/station/" + station._id);
   },
 };
--- routes.js
+++ routes.js
@@ -14,3 +14,4 @@ router.get("/about", aboutController.index);
 router.post("/dashboard/addstation", dashboardController.addStation);
 
 router.get("/station/:id", stationController.index);
+router.post("/station/:id/addreport", stationController.addReport);
